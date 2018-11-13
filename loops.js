bba1c38a8ce6b4fd9b16a48d3733fb354bef35f0
Angie Quin <aquinones100@gmail.com>
2018-11-13 16:35:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,6 +17,12 @@ function whileLoop(n){
 }
 
 function doWhileLoop(array){
-  
+  var i = 0
+  function incrementVariable (){
+    i = i + 1;
+  }
+  do {
+    array.pop()
+  } while (array.length > 0 && incrementVariable())
   return array
 }
